e1587eacc2f2409b55e8fcc7ee80a1f965b052a2
Lopunnii <kimi7@xtra.co.nz>
2022-02-22 17:42:02 +0900

    Createfunction to make 256 squares for 16 x 16 grid

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,4 +1,12 @@
-const gridContainer = document.querySelector('.grid-container');
-const square = document.createElement('div');
-square.className = "square"; 
-gridContainer.appendChild(square);
+function createSquareDiv () {
+    for(i=0; i<256; i++) {
+    const gridContainer = document.querySelector('.grid-container');
+    const square = document.createElement('div');
+    square.className = "square"; 
+    gridContainer.appendChild(square);
+    }
+}
+
+createSquareDiv();
+
+
