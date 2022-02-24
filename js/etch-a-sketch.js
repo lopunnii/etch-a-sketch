18fbe1d2bea8f8533267ca1f8fbbf4dcbd3ab4e6
Lopunnii <kimi7@xtra.co.nz>
2022-02-25 00:24:33 +0900

    Add mouse hover event listener to squares for drawing on the grid

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,12 +1,21 @@
 function createSquareDiv () {
     for(i=0; i<256; i++) {
-    const gridContainer = document.querySelector('.grid-container');
-    const square = document.createElement('div');
-    square.className = "square"; 
+    let gridContainer = document.querySelector('.grid-container');
+    let square = document.createElement('div');
+    square.className = 'square';
     gridContainer.appendChild(square);
     }
 }
 
 createSquareDiv();
 
+function hover () {
+    let squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = "black";
+        });
+    });
+}
 
+hover();
